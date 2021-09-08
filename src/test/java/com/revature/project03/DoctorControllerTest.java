1eea6113d0d3170d8bf419459189ccad91058d17
sreekamalcoc17 <sreekamalcoc17@gmail.com>
2021-09-08 15:44:55 +0530

    after adding testcases#02

--- src/test/java/com/revature/project03/DoctorControllerTest.java
+++ src/test/java/com/revature/project03/DoctorControllerTest.java
@@ -24,14 +24,23 @@ import org.junit.jupiter.api.BeforeEach;
 	import com.fasterxml.jackson.databind.ObjectMapper;
 	import com.revature.project03.controller.AppointmentController;
 import com.revature.project03.controller.DoctorController;
+import com.revature.project03.controller.LoginController;
 import com.revature.project03.controller.PatientController;
-	import com.revature.project03.entities.Appointment;
+import com.revature.project03.entities.Admin;
+import com.revature.project03.entities.Appointment;
 	import com.revature.project03.entities.Doctor;
 	import com.revature.project03.entities.Family;
-	import com.revature.project03.entities.Patient;
-	import com.revature.project03.service.AppointmentService;
+import com.revature.project03.entities.LoginRoute;
+import com.revature.project03.entities.Patient;
+import com.revature.project03.entities.Prescriptions;
+import com.revature.project03.model.Login;
+import com.revature.project03.model.Status;
+import com.revature.project03.service.AdminService;
+import com.revature.project03.service.AppointmentService;
 import com.revature.project03.service.DoctorService;
+import com.revature.project03.service.LoginRouteService;
 import com.revature.project03.service.PatientService;
+import com.revature.project03.service.PrescriptionService;
 
 	@ExtendWith(MockitoExtension.class)
 	public class DoctorControllerTest{
@@ -45,21 +54,27 @@ import com.revature.project03.service.PatientService;
 		@Mock
 		DoctorService doctorService;
 		
+		@InjectMocks
+		LoginController loginController;
+		
+		@Mock
+		LoginRouteService loginrouteservice;
+		
 		//@Mock
 		//AppointmentService appointmentService;
 		
-		private MockMvc mockDoctorMvc, mockMvc;
+		private MockMvc mockDoctorMvc, mockMvc, mockMvc1;
 		private ObjectMapper mapper;
 		
 		@BeforeEach
 		public void init() {
 			mockMvc = MockMvcBuilders.standaloneSetup(doctorController).build();
-			//mockMvc = MockMvcBuilders.standaloneSetup(patientController).build();
+			mockMvc1 = MockMvcBuilders.standaloneSetup(loginController).build();
 			mapper = new ObjectMapper();
 		}
 
 		@Test
-		public void testAddDoctors() throws Exception {
+		public void testGetDoctors() throws Exception {
 			
 			Doctor member=new Doctor();
 			member.setDoctorId(4);
@@ -93,5 +108,74 @@ import com.revature.project03.service.PatientService;
 			Doctor actualResponse = mapper.readValue(result.getResponse().getContentAsString(), Doctor.class);
 			assertEquals(member, actualResponse);
 		}
+		
+		@Test
+		public void testGetDoctorsbyemail() throws Exception {
+			
+			Doctor member=new Doctor();
+			member.setDoctorId(4);
+			member.setFirstName("Tester");
+			
+			
+			Mockito.when(
+				doctorService.getDoctorByEmail(Mockito.any())
+			).thenReturn(member);
+
+//			String postData = "{"
+//					+ "    \"doctorId\": 2,"
+//					+ "    \"firstName\": \"Sree\","
+//					+ "    \"lastName\": \"K\","
+//					+ "    \"email\": 21,"
+//					+ "    \"phno\": null,"
+//					+ "    \"fees\": \"8529638527\","
+//					+ "    \"designation\": \"Hyderabad\","
+//					+ "    \"specialization\": \"sree@gmail.com\","
+//					+ "		\"doctorAvailability\":pending \","
+//					+ "		\"password\":sree123 \","
+//					+ "}";
+			
+			// Send course as body to /students/Student1/courses
+			RequestBuilder requestBuilder = MockMvcRequestBuilders
+					.get("/doctorController/doctorByEmail/astha.pandya12@gmail.com");
+//					.accept(MediaType.APPLICATION_JSON).content(getData)
+//					.contentType(MediaType.APPLICATION_JSON);
+
+			MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+			Doctor actualResponse = mapper.readValue(result.getResponse().getContentAsString(), Doctor.class);
+			assertEquals(member, actualResponse);
+		}
+		
+//		@Test
+//		public void testloginfordoctor() throws Exception {
+//			
+//			LoginRoute member=new LoginRoute();
+//			member.setRouteId(2);
+//			member.setRole("Doctor");
+//			
+//
+//			Status status = new Status();
+//			status.setStat(true);
+//			
+//			
+//			
+//			Mockito.when(
+//				loginrouteservice.findbyEmail(Mockito.anyString())
+//			).thenReturn(member);
+//
+//			String postData = "{"
+//					+ "    \"email\": \"sreekamalcoc17@gmail.com\","
+//					+ "    \"password\": \"Y8b)*oU9FY\","
+//					+ "}";
+//			
+//			// Send course as body to /students/Student1/courses
+//			RequestBuilder requestBuilder = MockMvcRequestBuilders
+//					.post("/loginController/login")
+//					.accept(MediaType.APPLICATION_JSON).content(postData)
+//					.contentType(MediaType.APPLICATION_JSON);
+//
+//			MvcResult result = mockMvc1.perform(requestBuilder).andReturn();
+//			Status actualResponse = mapper.readValue(result.getResponse().getContentAsString(),Status.class);
+//			assertEquals(member, actualResponse);
+//		}
 	}
 	
